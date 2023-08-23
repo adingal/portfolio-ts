e71c50eb95d19440ef53450912cb65ee1274d969
Alex Dingal <xelalagnid@gmail.com>
2023-08-23 22:36:50 +0800

    Change google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'heading': ['Lobster', 'cursive'],
+        'heading': ['Montserrat', 'sans-serif'],
         'body': ['Lato', 'sans-serif'],
       },
     },
