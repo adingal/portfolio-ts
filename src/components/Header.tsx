6d4675617ec263825617ea4157078aa200f30d7c
Alex Dingal <xelalagnid@gmail.com>
2023-08-23 18:34:55 +0800

    Create responsive header

---
+++ src/components/Header.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+type HeaderLink = { id: number; name: string; url: string }
+type HeaderLinks = HeaderLink[]
+
+const headerLinks: HeaderLinks = [
+  { id: 0, name: 'Home', url: '#home' },
+  { id: 1, name: 'About', url: '#about' },
+  { id: 2, name: 'Portfolio', url: '#portfolio' },
+  { id: 3, name: 'Contact', url: '#contact' },
+]
+
+function Header(): JSX.Element {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
+
+  return (
+    <header className="bg-transparent py-6 md:py-8">
+      <div className="max-w-[1440px] mx-auto px-4">
+        <div className="flex flex-row items-center justify-between">
+          <div>
+            <p className="font-heading text-3xl md:text-4xl xl:text-5xl font-bold text-white first-letter:text-red-600">
+              Alex.
+            </p>
+          </div>
+          <div
+            className={`w-full lg:w-auto absolute lg:static bg-black lg:bg-transparent p-4 top-20 transition-all delay-300 ease-in-out ${
+              isMenuOpen ? 'left-0' : '-left-full'
+            }`}
+          >
+            {headerLinks?.length > 0 ? (
+              <ul className="flex flex-col lg:flex-row items-start lg:items-center justify-start space-y-3 lg:space-y-0 space-x-0 lg:space-x-10 font-body text-base md:text-lg xl:text-xl text-white">
+                {headerLinks.map((link) => (
+                  <li
+                    className="w-full pb-1 lg:pb-0 border-b border-gray-600 lg:border-b-0"
+                    key={link.id}
+                  >
+                    <a href={link.url}>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="text-sm text-white">
+                Please define links for header.
+              </p>
+            )}
+          </div>
+          <button
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className={`flex lg:hidden burger ${isMenuOpen ? 'show' : ''}`}
+            type="button"
+          >
+            <div className="strip burger-strip">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </button>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
